44580f25246cc48f2e731dbb9bee99252dfe6c22
Proxym <it_management@proxymgroup.com>
2026-01-17 10:44:48 +0100

    dashboard redis sse temps reel et API temps reel

--- resources/views/dashboards/index.blade.php
+++ resources/views/dashboards/index.blade.php
@@ -44,13 +44,12 @@
     <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-3">
         @php
             $types = [
-                'stolen'       => ['Stolen', 'fa-mask'],
-                'low_battery'  => ['Low Battery', 'fa-battery-quarter'],
+                'stolen'       => ['Vol', 'fa-mask'],
+                'low_battery'  => ['Batterie Faible', 'fa-battery-quarter'],
                 'geofence'     => ['Geofence', 'fa-draw-polygon'],
                 'safe_zone'    => ['Safe Zone', 'fa-shield-alt'],
-                'speed'        => ['Speed', 'fa-tachometer-alt'],
-                'engine'       => ['Engine', 'fa-engine-warning'],
-                'unauthorized' => ['Unauthorized', 'fa-clock'],
+                'speed'        => ['Vittesse', 'fa-tachometer-alt'],
+                'offline' => ['offline', 'fa-clock'],
                 'time_zone'    => ['Time Zone', 'fa-calendar-alt'],
             ];
         @endphp
